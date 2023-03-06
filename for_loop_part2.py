59ad849e67901a8dabebdca94c4f75d27d7297cb
Ruchitha Chinta <122854121+ruchitha36@users.noreply.github.com>
2023-03-06 19:59:48 +0530

    Add files via upload

---
+++ for_loop_part2.py
@@ -0,0 +1,44 @@
+# n=int(input("Enter any Integer Number:"))
+# for i in range(n):
+#     print(i)
+#     print(i)
+#     print(i)
+#     print("The Loop has been executed.")
+# print("Done")
+
+
+# n=int(input("Enter any Integer Number:"))
+# for i in range(1,n):
+#     print(i)
+
+# n=int(input("Enter any Integer Number:"))
+# for i in range(25,n):
+#     print(i,end=" ")
+ 
+# n=int(input("Enter any Integer Number:"))
+# for i in range(2,n,2):
+#     print(i)
+
+
+# b=int(input("Enter Any Integer Number:"))
+# if(b%2==0):
+#     print(f"{b} Even Number")
+# else:
+#     print(f"{b} Odd Number")
+
+
+# for i in range(1,5):
+#     print(i*i)
+
+# for i in range(1,5):
+#     print(i*i*i)
+
+# n=int(input("Enter the value of n:"))
+# while(n<=10):
+#     print("hello",str(n))
+#     n+=1
+
+# n=int(input("Enter the value of n:"))
+# while(n<=10):
+#     print("hello",str(n))
+#     n-=1
